2f42547f2c815cd89ea528fe80c89b8db2df1283
rosalybt <62813657+rosalybt@users.noreply.github.com>
2020-11-11 17:31:12 -0400

    feature: reiniciar juego y niveles los niveles es la dificultad de la combinacion de frutas

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
+const modalNuevoJuego = document.querySelector('.modal-nuevo-juego')
+const modalReiniciarJuego = document.querySelector('.modal-reinciar-juego')
+const botonFacil = document.getElementById('btn-facil')
+const botonMedio = document.getElementById('btn-medio')
+const botonDificil = document.getElementById('btn-dificil')
+const botonReiniciar = document.getElementById('btn-reiniciar')
+const botonReiniciarNuevo = document.getElementById('btn-reiniciar-juego')
+const botonCancelar = document.getElementById('btn-cancelar')
+
+let nivelDelJuego = ''
+const overlay = document.querySelector('.overlay')
+
+
+
+
+const hide = (element) => {
+    return element.classList.add('hidden');
+};
+
+const show = (element) => {
+    return element.classList.remove('hidden');
+};
+
+const addAClassToAnElement = (elemento, clase) => {
+    return elemento.classList.add(clase);
+}
+
 const grilla = document.querySelector(".contenedor-grilla")
 const items = ['🍉', '🍐', '🍌', '🍇', '🍎', '🍊'];
 let matriz = [];
 
 const obtenerNumeroAlAzar = (array) => {
     let numero = Math.floor((Math.random() * array.length));
-    console.log(numero)
     return numero;
 };
 
@@ -18,7 +44,7 @@ const obtenerFrutaAlAzar = (array) => {
 const crearGrilla = (filas, columnas, array) => {
     const anchoDeGrilla = 50 * columnas
     grilla.style.width = `${anchoDeGrilla}px`
-
+    grilla.innerHTML = ''
     matriz = [];
 
     for (let i = 0; i < filas; i++) {
@@ -36,22 +62,70 @@ const crearGrilla = (filas, columnas, array) => {
 
 
 
-crearGrilla(9, 9, items)
-// switch (dificultad.toLowerCase()) {
-//     case "facil":
-//         crearGrilla(9, 9, items)
-//         break;
+const nuevoJuego = () => {
+    show(overlay)
+    show(modalNuevoJuego)
 
-//     case "media":
-//         crearGrilla(6, 6)
-//         break;
-//     case "dificil":
-//         crearGrilla(4, 4)
-//         break;
+};
 
-//     default:
-//         break;
-// }
+const reiniciarJuego = () => {
+    hide(overlay)
+    hide(modalReiniciarJuego)
+    switch (nivelDelJuego) {
+        case 'facil':
+            crearGrilla(9, 9, items);
+            break;
+
+        case 'medio':
+            crearGrilla(6, 6, items);
+            break;
+
+        case 'dificil':
+            crearGrilla(4, 4, items);
+            break;
+
+        default:
+            alert('Debe seleccionar un nivel')
+            break;
+    }
+
+};
+
+botonCancelar.onclick = () => {
+    hide(overlay)
+    hide(modalReiniciarJuego)
+}
+
+botonReiniciar.onclick = () => {
+    show(overlay)
+    show(modalReiniciarJuego)
+
+}
+
+botonReiniciarNuevo.onclick = () => {
+    reiniciarJuego();
+}
+
+botonFacil.onclick = () => {
+    crearGrilla(9, 9, items)
+    hide(modalNuevoJuego)
+    hide(overlay)
+    nivelDelJuego = 'facil'
+}
+
+botonMedio.onclick = () => {
+    crearGrilla(6, 6, items)
+    hide(modalNuevoJuego)
+    hide(overlay)
+    nivelDelJuego = 'medio'
+}
+
+botonDificil.onclick = () => {
+    crearGrilla(4, 4, items)
+    hide(modalNuevoJuego)
+    hide(overlay)
+    nivelDelJuego = 'dificil'
+}
 
 
 
